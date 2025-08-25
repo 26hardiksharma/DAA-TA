9531223c2316cecc38e8b853433fa80698c10463
EternalSlayer <hardikslayer2005@gmail.com>
2025-08-25 13:46:16 +0530

    Added TarjansAlgorithm.java and MostStonesRemoved.java

---
+++ MostStonesRemoved.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MostStonesRemoved {
+	static class UnionFind {
+		private Map<Integer, Integer> parent = new HashMap<>();
+		public int find(int x) {
+			if (!parent.containsKey(x)) parent.put(x, x);
+			if (parent.get(x) != x) parent.put(x, find(parent.get(x)));
+			return parent.get(x);
+		}
+		public void union(int x, int y) {
+			parent.put(find(x), find(y));
+		}
+		public int count() {
+			Set<Integer> roots = new HashSet<>();
+			for (int x : parent.keySet()) roots.add(find(x));
+			return roots.size();
+		}
+	}
+
+	public static int removeStones(int[][] stones) {
+		UnionFind uf = new UnionFind();
+		for (int[] stone : stones) {
+			uf.union(stone[0], stone[1] + 10001);
+		}
+		return stones.length - uf.count();
+	}
+
+	public static void main(String[] args) {
+		int[][] stones = {
+			{0, 0}, {0, 2}, {1, 3}, {3, 1}, {3, 2}, {4, 3}
+		};
+		int result = removeStones(stones);
+		System.out.println("Maximum stones that can be removed: " + result);
+	}
+}
+
---
+++ TarjansAlgorithm.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class TarjansAlgorithm {
+    private int time = 0;
+
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (List<Integer> conn : connections) {
+            int u = conn.get(0), v = conn.get(1);
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int[] disc = new int[n];
+        int[] low = new int[n];
+        Arrays.fill(disc, -1);
+
+        for (int i = 0; i < n; i++) {
+            if (disc[i] == -1) {
+                dfs(i, -1, graph, disc, low, result);
+            }
+        }
+        return result;
+    }
+
+    private void dfs(int u, int parent, List<Integer>[] graph, int[] disc, int[] low, List<List<Integer>> result) {
+        disc[u] = low[u] = time++;
+        for (int v : graph[u]) {
+            if (v == parent) continue;
+            if (disc[v] == -1) {
+                dfs(v, u, graph, disc, low, result);
+                low[u] = Math.min(low[u], low[v]);
+                if (low[v] > disc[u]) {
+                    result.add(Arrays.asList(u, v));
+                }
+            } else {
+                low[u] = Math.min(low[u], disc[v]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        List<List<Integer>> connections = new ArrayList<>();
+        connections.add(Arrays.asList(0, 1));
+        connections.add(Arrays.asList(1, 2));
+        connections.add(Arrays.asList(2, 0));
+        connections.add(Arrays.asList(1, 3));
+        TarjansAlgorithm ta = new TarjansAlgorithm();
+        List<List<Integer>> bridges = ta.criticalConnections(n, connections);
+        System.out.println("Critical Connections: " + bridges);
+    }
+}
